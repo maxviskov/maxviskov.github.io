6a7f8f53789944430c0d2f2804c5b54f5bd921e0
Max Viskov <maxviskov@gmail.com>
2024-05-19 02:47:42 +0000

    Added DOM manipulation

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,16 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+    });
+  });
 }
